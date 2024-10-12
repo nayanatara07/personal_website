b83185662ac5268623dd69dfafc4de4d4a0d864c
Nayanatara <nayana2017tara@gmail.com>
2024-10-12 17:06:47 +0530

    usong the components in app.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import './About.css';
+import React, { useState } from 'react';
+import './App.css';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import Navbar from './Navbar'; 
+import About from './About';   
 
-function About() {
+function App() {
+  const [theme, setTheme] = useState('dark-mode');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark-mode' ? 'light-mode' : 'dark-mode');
+  };
+
   return (
-    <section className="about-section">
-      <div className="avatar">
-        <img src="./avatar.png" alt="avatar" />
+    <div className={`App ${theme}`}>
+      <Navbar /> 
+      <About />  
+      <div className="theme-toggle" onClick={toggleTheme}>
+        {theme === 'dark-mode' ? (
+          <FaSun className="icon" />
+        ) : (
+          <FaMoon className="icon" />
+        )}
       </div>
-      <h1>NayanaTara</h1>
-      <h2>Software Developer | Tech Enthusiast</h2>
-      <ul className="social-links">
-        <li>
-          <a href="https://github.com/NayanaTara" target="_blank" rel="noopener noreferrer" aria-label="Github">
-            <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/NayanaTara_" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-            <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://linkedin.com/in/nayana-tara" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-            <i className="fa fa-linkedin fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-      <p><a href="/posts/" aria-label="Explore more">Explore </a></p>
-    </section>
+    </div>
   );
 }
 
-export default About;
+export default App;
